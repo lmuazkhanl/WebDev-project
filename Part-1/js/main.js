2a5f4cce2ba21d98cb28801b68167ce67a24781f
Abdullah Nawaz <nawazabdullah470@gmail.com>
2024-03-12 00:25:25 +0300

    Login Form Almost Done

--- Part-1/js/main.js
+++ Part-1/js/main.js
@@ -15,3 +15,30 @@ navClose.addEventListener("click", () => {
   navLinks.classList.remove("show_menu");
 });
 
+/* change background color in login page based on selectedvalue */
+document.addEventListener("DOMContentLoaded", function () {
+  const loginTypeSelect = document.getElementById("loginType");
+  const formContainer = document.querySelector(".form-container");
+
+  loginTypeSelect.addEventListener("change", function () {
+    const selectedValue = this.value;
+    let backgroundImage = "";
+
+    switch (selectedValue) {
+      case "customer":
+        backgroundImage =
+          'url("../Images/login/customer_login_background.jpg")';
+        break;
+      case "seller":
+        backgroundImage = 'url("../Images/login/seller_login_background.jpg")';
+        break;
+      case "admin":
+        backgroundImage = 'url("../Images/login/admin_login_background.jpg")';
+        break;
+      default:
+        backgroundImage = "";
+    }
+
+    formContainer.style.backgroundImage = backgroundImage;
+  });
+});
