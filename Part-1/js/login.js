4113a01a1fc332e4a4e613ac136cf5e810d2917c
Abdullah Nawaz <nawazabdullah470@gmail.com>
2024-03-16 00:30:25 +0300

    Add Search Results Page

--- Part-1/js/login.js
+++ Part-1/js/login.js
@@ -1,28 +1,32 @@
-/* change background color in login page based on selectedvalue */
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
+  /* change background color in login page based on selectedvalue */
+  const loginTypeSelect = document.querySelector("#loginType");
+  const formContainer = document.querySelector(".form-container");
 
-    const loginTypeSelect = document.getElementById("loginType");
-    const formContainer = document.querySelector(".form-container");
-  
-    loginTypeSelect.addEventListener("change", function () {
-      const selectedValue = this.value;
-      let backgroundImage = "";
-  
-      switch (selectedValue) {
-        case "customer":
-          backgroundImage =
-            'url("../Images/login/customer_login_background.jpg")';
-          break;
-        case "seller":
-          backgroundImage = 'url("../Images/login/seller_login_background.jpg")';
-          break;
-        case "admin":
-          backgroundImage = 'url("../Images/login/admin_login_background.jpg")';
-          break;
-        default:
-          backgroundImage = "";
-      }
-  
-      formContainer.style.backgroundImage = backgroundImage;
-    });
-  });
+  loginTypeSelect.addEventListener("change", switchBackgroundLogin);
+
+  function switchBackgroundLogin() {
+    const selectedValue = loginTypeSelect.value;
+    let backgroundImage = "";
+    switch (selectedValue) {
+      case "customer":
+        backgroundImage =
+          'url("../Images/login/customer_login_background.jpg")';
+        break;
+      case "seller":
+        backgroundImage = 'url("../Images/login/seller_login_background.jpg")';
+        break;
+      case "admin":
+        backgroundImage = 'url("../Images/login/admin_login_background.jpg")';
+        break;
+      default:
+        backgroundImage = "";
+    }
+    formContainer.style.backgroundImage = backgroundImage;
+  }
+
+  /* Implement Login Functionality */
+  const userType = document.querySelector("#loginType");
+  const username = document.querySelector("#username");
+  const password = document.querySelector("#password");
+});
