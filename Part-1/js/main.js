f927ffcb5f81329dd2a75094cf799569cd6599ab
Abdullah Nawaz <nawazabdullah470@gmail.com>
2024-03-30 20:55:43 +0300

    Implement Female Category Cards

--- Part-1/js/main.js
+++ Part-1/js/main.js
@@ -153,3 +153,104 @@ document.querySelector(".men_accessories").addEventListener("click", function (e
     localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
     window.location.href = "searchItems.html";
 });
+
+// Women Hoodie section
+document.querySelector(".women_hoodie").addEventListener("click", function (event) {
+    event.preventDefault();
+    var searchQuery = "hoodie";
+    localStorage.setItem("searchQuery", searchQuery);
+
+    const items = JSON.parse(localStorage.getItem("items"));
+    const filteredItems = items.filter((item) => {
+        return (
+            item.gender.toLowerCase() === "female" &&
+            (item.name.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes(searchQuery) ||
+                item.material.toLowerCase().includes(searchQuery) ||
+                item.description.toLowerCase().includes(searchQuery))
+        );
+    });
+    localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
+    window.location.href = "searchItems.html";
+});
+
+// Women Sweatshirt section
+document.querySelector(".women_sweatshirt").addEventListener("click", function (event) {
+    event.preventDefault();
+    var searchQuery = "sweatshirt";
+    localStorage.setItem("searchQuery", searchQuery);
+
+    const items = JSON.parse(localStorage.getItem("items"));
+    const filteredItems = items.filter((item) => {
+        return (
+            item.gender.toLowerCase() === "female" &&
+            (item.name.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes(searchQuery) ||
+                item.material.toLowerCase().includes(searchQuery) ||
+                item.description.toLowerCase().includes(searchQuery))
+        );
+    });
+    localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
+    window.location.href = "searchItems.html";
+});
+
+// Women Pants section
+document.querySelector(".women_pants").addEventListener("click", function (event) {
+    event.preventDefault();
+    var searchQuery = "pant";
+    localStorage.setItem("searchQuery", searchQuery);
+
+    const items = JSON.parse(localStorage.getItem("items"));
+    const filteredItems = items.filter((item) => {
+        return (
+            item.gender.toLowerCase() === "female" &&
+            (item.name.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes(searchQuery) ||
+                item.material.toLowerCase().includes(searchQuery) ||
+                item.description.toLowerCase().includes(searchQuery))
+        );
+    });
+    localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
+    window.location.href = "searchItems.html";
+});
+
+// Women Shoes section
+document.querySelector(".women_shoes").addEventListener("click", function (event) {
+    event.preventDefault();
+    var searchQuery = "shoe";
+    localStorage.setItem("searchQuery", searchQuery);
+
+    const items = JSON.parse(localStorage.getItem("items"));
+    const filteredItems = items.filter((item) => {
+        return (
+            item.gender.toLowerCase() === "female" &&
+            (item.name.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes(searchQuery) ||
+                item.material.toLowerCase().includes(searchQuery) ||
+                item.description.toLowerCase().includes(searchQuery))
+        );
+    });
+    localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
+    window.location.href = "searchItems.html";
+});
+
+// Women Accessories section
+document.querySelector(".women_accessories").addEventListener("click", function (event) {
+    event.preventDefault();
+    var searchQuery = "accessories";
+    localStorage.setItem("searchQuery", searchQuery);
+
+    const items = JSON.parse(localStorage.getItem("items"));
+    const filteredItems = items.filter((item) => {
+        return (
+            item.gender.toLowerCase() === "female" &&
+            (item.name.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes(searchQuery) ||
+                item.type.toLowerCase().includes("jewelery") ||
+                item.material.toLowerCase().includes(searchQuery) ||
+                item.description.toLowerCase().includes(searchQuery))
+        );
+    });
+    localStorage.setItem("filteredItems", JSON.stringify(filteredItems));
+    window.location.href = "searchItems.html";
+});
