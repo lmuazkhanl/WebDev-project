cf2e73380c25d2d4319c94fd11327db91fed0719
Abdullah Nawaz <nawazabdullah470@gmail.com>
2024-04-27 13:19:09 +0300

    Change Database Scema (PurchaseHistory -> Purchase)

--- my-app/app/api/statistics/[stat]/route.js
+++ my-app/app/api/statistics/[stat]/route.js
@@ -9,6 +9,8 @@ export async function GET(request, { params }) {
 
     if (requestedStat === "most-successful-sellers") {
         data = await getMostSuccessfulSellers();
+    } else if (requestedStat === "most-popular-products") {
+        data = await getMostPopularProducts();
     }
 
     return Response.json(data, {
@@ -37,3 +39,36 @@ async function getMostSuccessfulSellers() {
         moneyMade: seller.bank_account_balance,
     }));
 }
+
+async function getMostPopularProducts() {
+    const mostPopularProducts = await prisma.item.findMany({
+        orderBy: {
+            PurchaseHistory: {
+                _sum: {
+                    quantity: "desc",
+                },
+            },
+        },
+        take: 5,
+        select: {
+            name: true,
+            image: true,
+            price: true,
+            PurchaseHistory: {
+                select: {
+                    quantity: true,
+                },
+            },
+        },
+    });
+
+    return mostPopularProducts.map((product) => ({
+        name: product.name,
+        image: product.image,
+        quantitySold: product.PurchaseHistory.reduce((total, purchase) => total + purchase.quantity, 0),
+        moneyMade: product.PurchaseHistory.reduce((total, purchase) => total + purchase.quantity * product.price, 0),
+    }));
+}
+
+const date = await getMostPopularProducts();
+console.log(date);
--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import SuccessfulSellers from "./Statistics-Components/SuccessfulSellers";
+import MostPopularProducts from "./Statistics-Components/MostPopularProducts";
 /* import './global.css' */
 
 export default function Home() {
   return (
     <>
     <SuccessfulSellers/>
+    <hr />
+    <MostPopularProducts/>
     </>
   );
 }
