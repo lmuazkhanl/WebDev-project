cc8b415e20bebde59ecd34b04a0705654f8c898e
lmuazkhanl <147280851+lmuazkhanl@users.noreply.github.com>
2024-05-08 00:02:03 +0300

    converting localStorage to Api fetch
    did women-page.js , search.js

--- my-app/app/api/products/[id]/route.js
+++ my-app/app/api/products/[id]/route.js
@@ -2,16 +2,30 @@ import ProductsRepo from "@/app/repo/products-repo";
 const productsRepo = new ProductsRepo();
 
 export async function GET(request, { params }) {
-    const productId = params.id;
-    const product = await productsRepo.getProduct(productId);
+	const productId = params.id;
+	const product = await productsRepo.getProduct(productId);
 
-    return Response.json({ search: product }, { status: 200 });
+	// return Response.json({ search: product }, { status: 200 });
+	return new Response(JSON.stringify(products), {
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+		status: 200,
+	});
 }
 
 export async function PUT(request, { params }) {
-    const productUpdate = await request.json();
-    const accountNo = params.id;
+	const productUpdate = await request.json();
+	const accountNo = params.id;
 
-    const message = await productsRepo.updateProduct(productUpdate, accountNo);
-    return Response.json(message, { status: 200 });
+	const message = await productsRepo.updateProduct(productUpdate, accountNo);
+	// return Response.json(message, { status: 200 });
+	return new Response(JSON.stringify(newProduct), {
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+		status: 201,
+	});
 }
